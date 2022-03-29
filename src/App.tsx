ff8d5b50ea37f6aa303e0451a69a64ad3474054f
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-03-28 20:19:32 -0700

    Typing effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+import Typed from 'react-typed';
+
 
 export default function App() {
     return (
         <>
             <Header />
             <div className="center bold">
-                <p>Bobo Bot, The Anime Bot but better.</p>
+                <Typed strings={['Bobo Bot, The Anime Bot but better.', 'Did I mention but better?', 'Yes I did.']} typeSpeed={40} backSpeed={50} />
             </div>
+
             <Footer />
         </>
     )

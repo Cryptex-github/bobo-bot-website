754526ee640ef6e761d8475f0e9a9bddf548c288
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-04-24 19:26:36 -0700

    API

---
+++ src/Api.tsx
@@ -0,0 +1,129 @@
+import { createContext } from 'react';
+import Cookies from 'js-cookie';
+
+const BASE_URL = 'https://api.bobobot.cf';
+
+type OauthData = {
+    access_token: string,
+    token_type: string,
+    expires_in: number,
+    refresh_token: string,
+    scope: string
+}
+
+type UserData = {
+    id: string,
+    username: string,
+    discriminator: string,
+    avatar?: string,
+    bot: Boolean,
+    mfa_enabled?: boolean,
+    locale?: string,
+    verified?: boolean,
+    email?: string,
+    flags?: number,
+    premium_type?: number,
+    public_flags?: number
+}
+
+type Method = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' | 'HEAD' | 'OPTIONS';
+
+export class APIClient {
+    OauthData: OauthData | null;
+    UserData: UserData | null;
+    Authorization: string | null;
+
+    constructor() {
+        this.OauthData = null;
+        this.UserData = null;
+        this.Authorization = null;
+    }
+
+    async getAccessToken(): Promise<string> {
+        let accessToken = Cookies.get('access_token');
+
+        if (accessToken) {
+            // TODO: fetch user
+
+            return accessToken;
+        }
+
+        const params = new URLSearchParams(window.location.search);
+
+        if (params.has('code')) {
+            const code = params.get('code')!;
+            await this.exchangeCode(code);
+
+            Cookies.set('access_token', this.OauthData!.access_token, { expires: this.OauthData!.expires_in / 86400 });
+            Cookies.set('refresh_token', this.OauthData!.refresh_token, { expires: this.OauthData!.expires_in / 86400 });
+        }
+
+        return this.OauthData!.access_token;
+    }
+
+    async getUser(): Promise<UserData> {
+        if (this.UserData != null) {
+            return this.UserData;
+        }
+
+        let userData = Cookies.get('user_data');
+
+        if (userData) {
+            this.UserData = JSON.parse(userData);
+            return this.UserData!;
+        }
+
+        const data = await this.request('GET', '/discord/user');
+
+        this.UserData = data;
+        Cookies.set('user_data', JSON.stringify(data), { expires: 1 });
+
+        return data;
+    }
+
+    async exchangeCode(code: string): Promise<OauthData> {
+        const data = await this.request('POST', '/oauth2/token', { auth: false, params: { code } });
+        this.OauthData = data;
+
+        return data
+    }
+
+    async request(
+        method: Method, 
+        route: string, 
+        { json, params, auth, headers }: 
+        { json?: any, params?: any, auth: boolean, headers?: any } = 
+        { auth: true }
+        ): Promise<any> {
+        let body;
+
+        if (json) {
+            headers['Content-Type'] = 'application/json';
+            body = JSON.stringify(json);
+        }
+
+        if (auth) {
+            // Ensure we have an Authorization token
+
+            headers['Authorization'] = this.Authorization;
+        }
+
+        const resp = await fetch(BASE_URL + route + (params ? '?' + new URLSearchParams(params).toString() : ''), { method, headers, body });
+        const text = await resp.text();
+        const message = `${method} ${route} received status code ${resp.status} with content: ${text}`
+
+        if (!resp.ok) {
+            console.error(message);
+            throw new Error(message);
+        }
+        
+        if (resp.headers.get('Content-Type') === 'application/json') {
+            return JSON.parse(text);
+        }
+
+        return text;
+    }
+}
+
+export const DefaultAPIClient = new APIClient();
+export const APIClientContext = createContext<APIClient>(DefaultAPIClient);

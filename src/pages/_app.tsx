4b77ddbfc15df9ff0613d0bfdc6bb1df3786a18f
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-04-26 21:14:52 -0700

    Change icon color and alignment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { IconContext } from 'react-icons';
 import type { AppProps } from 'next/app'
 import { createGlobalStyle } from 'styled-components';
 import Head from 'next/head';
@@ -26,13 +27,15 @@ export default function App({ Component, pageProps }: AppProps) {
         <React.StrictMode>
             <GlobalStyle />
             <APIClientContext.Provider value={DefaultAPIClient}>
-                <Head>
-                    <title>Bobo Bot</title>
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                </Head>
-                <Header/>
-                <Component {...pageProps} />
-                <Footer/>
+                <IconContext.Provider value={{ color: '#FFFFFF', style: { verticalAlign: 'middle' } }}>
+                    <Head>
+                        <title>Bobo Bot</title>
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    </Head>
+                    <Header/>
+                    <Component {...pageProps} />
+                    <Footer/>
+                </IconContext.Provider>
             </APIClientContext.Provider>
         </React.StrictMode>
     )

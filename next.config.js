38e816ee55108ae9e418a336be0cc353d8d074b1
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-04-26 19:28:19 -0700

    Security headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+    {
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on'
+    },
+    {
+        key: 'X-XSS-Protection',
+        value: '1; mode=block'
+    },
+    {
+        key: 'X-Frame-Options',
+        value: 'SAMEORIGIN'
+    },
+    {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff'
+    }
+]
+
+if (process.env.NODE_ENV === 'production') {
+    securityHeaders.push(
+        {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+        },
+        {
+            key: 'Content-Security-Policy',
+            value: `default-src https: 'unsafe-inline'`
+        }
+    )
+}
+
 const nextConfig = {
     reactStrictMode: true,
     compiler: {
@@ -6,6 +39,15 @@ const nextConfig = {
     },
     images: {
         domains: ['cdn.bobobot.cf'],
+    },
+
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: securityHeaders,
+            }
+        ]
     }
 }
   

60e78df215a336ee852b95e72e5054002dc3a844
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-04-29 18:58:06 -0700

    webkit compat

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -7,9 +7,12 @@ const Title = styled.h1`
     display: table;
     background-image: linear-gradient(20deg, #8500ff, #ff8100);
     filter: drop-shadow(0 20px 30px #28d8ff33);
-    -webkit-text-fill-color: transparent;
     background-clip: text;
     box-decoration-break: clone;
+    -webkit-text-fill-color: transparent;
+    -webkit-background-clip: text;
+    -webkit-box-decoration-break: clone;
+
 `
 
 const Text = styled.h2`
